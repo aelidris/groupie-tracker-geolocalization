e64763d35125cdb86cdd0514c5a6c7bbdcf3f584
aelidris <smias1.1998@gmail.com>
2024-09-16 19:17:42 +0100

    geo

--- hundlers/errors.go
+++ hundlers/errors.go
@@ -14,10 +14,12 @@ func renderErrorPage(w http.ResponseWriter, statusCode int, message string) {
 	tmpl, err := template.ParseFiles("views/error.html")
 	if err != nil {
 		http.Error(w, "Error loading error page", http.StatusInternalServerError)
+		return
 	}
 
 	err = tmpl.Execute(w, errorData)
 	if err != nil {
 		http.Error(w, "Error rendering error page", http.StatusInternalServerError)
+		return
 	}
 }
